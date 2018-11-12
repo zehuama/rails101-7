99ef9458f08396d6b50baa3f3d4e115f76b78160
javiel <zehua046@gmail.com>
2018-11-12 10:29:23 +0800

    implement posts#CRUD

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,45 @@
 class PostsController < ApplicationController
+  def new
+    @group = Group.find(params[:group_id])
+    @post = Post.new
+  end
+
+  def edit
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+  end
+
+  def create
+    @group = Group.find(params[:group_id])
+    @post = Post.new(post_params)
+    @post.group = @group
+    if @post.save
+      redirect_to group_path(@group), notice: "新增文章成功！"
+    else
+      render :new
+    end
+  end
+
+  def update
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to group_path(@group), notice: "文章修改成功！"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to group_path(@group), alert: "文章已删除"
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ApplicationRecord
+  validates :content, presence: true
   belongs_to :group
 end
