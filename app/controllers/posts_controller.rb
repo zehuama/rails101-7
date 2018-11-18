3949d5cd036fe6eb772c81f5cc893f1b4bfc6db7
javiel <zehua046@gmail.com>
2018-11-18 10:32:59 +0800

    only creater can edit/destroy post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,18 +1,23 @@
 class PostsController < ApplicationController
   before_action :find_group
   before_action :authenticate_user!
-  
+
   def new
-        @post = Post.new
+    @post = Post.new
   end
 
   def edit
     @post = Post.find(params[:id])
+
+    if current_user != @post.user
+      redirect_to group_path(@group), alert: "You have no permission."
+    end
   end
 
   def create
     @post = Post.new(post_params)
     @post.group = @group
+    @post.user = current_user
     if @post.save
       redirect_to group_path(@group), notice: "新增文章成功！"
     else
@@ -22,6 +27,11 @@ class PostsController < ApplicationController
 
   def update
     @post = Post.find(params[:id])
+    @post.group = @group
+    if current_user != @post.user
+      redirect_to group_path(@group), alert: "You have no permission."
+    end
+    @post.user = current_user
     if @post.update(post_params)
       redirect_to group_path(@group), notice: "文章修改成功！"
     else
@@ -31,8 +41,12 @@ class PostsController < ApplicationController
 
   def destroy
     @post = Post.find(params[:id])
-    @post.destroy
-    redirect_to group_path(@group), alert: "文章已删除"
+    if current_user != @post.user
+      redirect_to group_path(@group), alert: "You have no permission."
+    else
+      @post.destroy
+      redirect_to group_path(@group), alert: "文章已删除"
+    end
   end
 
   private
