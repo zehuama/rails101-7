43b1ec60206781f2179a3beaafeda1a201d688d2
javiel <zehua046@gmail.com>
2018-11-20 21:54:00 +0800

    refactor query by scope

--- app/controllers/account/posts_controller.rb
+++ app/controllers/account/posts_controller.rb
@@ -2,7 +2,7 @@ class Account::PostsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @posts = current_user.posts.order("updated_at DESC")
+    @posts = current_user.posts.recent
   end
 
   def edit
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ApplicationRecord
   validates :content, presence: true
   belongs_to :group
-  belongs_to :user 
+  belongs_to :user
+
+  scope :recent, -> { order("updated_at DESC")}
 end
