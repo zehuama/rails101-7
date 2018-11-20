3e7ae17b5db2c6841bfd588d00ed4c901a4e783a
javiel <zehua046@gmail.com>
2018-11-20 21:40:09 +0800

    refactor code for views

--- app/controllers/account/posts_controller.rb
+++ app/controllers/account/posts_controller.rb
@@ -2,7 +2,7 @@ class Account::PostsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @posts = current_user.posts
+    @posts = current_user.posts.order("updated_at DESC")
   end
 
   def edit
